0636552020442c8035bdb6841b32b5c437211c25
damiralmaev <dam.almaev@gmail.com>
2020-02-21 17:25:27 +0300

    Ненавижу FTP

--- Console/BuildServer/FTP.cs
+++ Console/BuildServer/FTP.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Primitives;
 using FubarDev.FtpServer.FileSystem.DotNet;
 using System.IO;
 using System.Threading;
+using System.Security.Cryptography.X509Certificates;
 
 namespace BuildServer
 {
@@ -17,14 +18,24 @@ namespace BuildServer
     {
         static public void Start()
         {
+            /* 1 */
             var servis = new ServiceCollection();
 
             servis.Configure<DotNetFileSystemOptions>(opt => { opt.RootPath = "Test"; });
             servis.AddFtpServer(builder => { builder.UseDotNetFileSystem().EnableAnonymousAuthentication();});
 
             //servis.Configure<FtpServerOptions>(opt => { opt.ServerAddress = "127.0.0.1"; opt.Port = 987; opt.MaxActiveConnections = 10; });
-            servis.ConfigureAll<FtpServerOptions>(opt => { opt.Port = 987; opt.MaxActiveConnections = 10; });
+            servis.ConfigureAll<FtpServerOptions>(opt => { opt.Port = 987; opt.ConnectionInactivityCheckInterval = new TimeSpan(0, 0, 10); opt.MaxActiveConnections = 10; });
             //servis.PostConfigureAll<PostConfigureOptions<AuthTlsOptions>>(opt => { opt.});
+
+            // Это нужно простестить!
+            var cert = new X509Certificate2("localhost.pfx", "password");
+            servis.Configure<AuthTlsOptions>(cfg => {
+                cfg.ServerCertificate = cert;
+                cfg.ImplicitFtps = false;
+            });
+            //
+
             servis.AddOptions();
             using (var servisprod = servis.BuildServiceProvider())
             { 
@@ -33,6 +44,56 @@ namespace BuildServer
                 Console.ReadLine();
                 ftpserverHost.StopAsync(CancellationToken.None).Wait();
             }
+            
+
+            /* 2
+
+            // This example requires the Chilkat API to have been previously unlocked.
+            // See Global Unlock Sample for sample code.
+
+            Chilkat.Ftp2 ftp = new Chilkat.Ftp2();
+            
+
+            // If this example does not work, try using passive mode
+            // by setting this to true.
+            ftp.Passive = true;
+
+            ftp.Hostname = "127.0.0.1";
+            //ftp.Username = "Gladi";
+            //ftp.Port = 987;
+            //ftp.Password = "test";
+
+            // Establish an AUTH TLS secure channel after connection
+            // on the standard FTP port 21.
+            //ftp.AuthTls = true;
+
+            // The Ssl property is for establishing an implicit SSL connection
+            // on port 990.  Do not set it.
+            //ftp.Ssl = false;
+
+            // Connect and login to the FTP server.
+            
+            //if (success != true)
+            //{
+            //    Console.WriteLine(ftp.LastErrorText);
+            //    //return;
+            //}
+            //else
+            //{
+            //    // LastErrorText contains information even when
+            //    // successful. This allows you to visually verify
+            //    // that the secure connection actually occurred.
+            //    Console.WriteLine(ftp.LastErrorText);
+            //}
+
+            Console.WriteLine("Secure FTP Channel Established!");
+
+            // Do whatever you're doing to do ...
+            // upload files, download files, etc...
+
+            //success = ftp.Disconnect();
+            Console.ReadLine();
+            */
         }
 
         static public void Stop()
