c508db59d85842e09c82db744e0a110ed193cc06
Gladi <dam.almaev@gmail.com>
2020-02-29 12:49:13 +0300

    Error!

--- Console/BuildServer/Functions.cs
+++ Console/BuildServer/Functions.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace BuildServer
 {
     static public class Functions
     {
+        static public void WriteLog(string text, ConsoleColor color)//ещё не готово!
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine($"{DateTime.Now}: {text}");
+            
+            Console.ResetColor();
+        }
+
         static public void WriteLine(string text, ConsoleColor color)
         {
             Console.ForegroundColor = color;
