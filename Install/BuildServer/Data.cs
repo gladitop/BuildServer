5ae2f3d2628a244153adb66c6a1c85f598ce0ddb
Gladi <dam.almaev@gmail.com>
2020-03-01 16:19:31 +0300

    Установщик (1 часть)

---
+++ Install/BuildServer/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildServer
+{
+    static public class Data
+    {
+        static public int Number { get; set; } = 1;
+        static public bool NewPages { get; set; } = false;
+    }
+}
--- Install/BuildServer/MainWindow.xaml.cs
+++ Install/BuildServer/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace BuildServer
 {
@@ -20,9 +21,42 @@ namespace BuildServer
     /// </summary>
     public partial class MainWindow : Window
     {
+        object page = null;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            page = new Pages.Welcom();
+            frame.Navigate(page);
+            Thread thread = new Thread(new ThreadStart(Update));
+            thread.Start();
+        }
+
+        public void Update()
+        {
+            while (true)
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    Task.Delay(10).Wait();
+
+                    if (Data.NewPages == true)
+                    {
+                        if (Data.Number == 2)
+                        {
+                            page = new Pages.Installing();
+                            frame.Navigate(page);
+                            Data.NewPages = false;
+                        }
+                    }
+                }));
+            }
+        }
+
+        private void mainWindows_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.Close();
         }
     }
 }
