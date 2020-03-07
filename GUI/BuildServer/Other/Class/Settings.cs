d91db087da73f5ae9f2435cbfc99df0a63feec3b
Gladi <dam.almaev@gmail.com>
2020-03-07 14:59:43 +0300

    Json and update packages

---
+++ GUI/BuildServer/Other/Class/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BuildServer.Other.Class
+{
+    public class Settings
+    {
+        [JsonProperty("startProgram")]
+        static public bool StartProgram { get; set; }
+        [JsonProperty("autoStart")]
+        static public bool AutoStart { get; set; }
+        [JsonProperty("normalStart")]
+        static public bool NormalStart { get; set; }
+    }
+}
---
+++ GUI/BuildServer/Other/Class/SettingsManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace BuildServer.Other.Class
+{
+    public class SettingsManager
+    {
+
+        public void Settings()
+        {
+            
+        }
+    
+        static public void Save()
+        {
+            if (File.Exists("Settings.json"))
+            {
+                var _settigs = JsonConvert.DeserializeObject<SettingsForm>("Settings.json");//Нужен нормальный класс!
+            }
+        }
+
+        static public void Load()
+        {
+            
+        }
+    }
+}
